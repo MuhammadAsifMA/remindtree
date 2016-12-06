8aa8ef9659a37eda7458627128c661f4b83294e5
Muhammad Asif <muhammadasif.mmma@gmail.com>
2016-12-06 17:20:04 +0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'sql106.malikasif.xyz'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'mystic'),
+            'username' => env('DB_USERNAME', 'mlks_18359598'),
+            'password' => env('DB_PASSWORD', 'malikasif'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
